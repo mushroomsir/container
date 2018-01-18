4dc2cc19a8fc20d84fea5b2f5ae3bec19ec7cd53
pengwei <pengwei@teambition.com>
2018-01-18 16:00:43 +0800

    feat: add network

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/exec"
 	"strconv"
 	"syscall"
+
+	"github.com/mushroomsir/container/network"
 )
 
 func main() {
@@ -15,15 +18,40 @@ func main() {
 		parent()
 	case "child":
 		child()
+	case "bridge":
+		createBridge(os.Args[2])
 	default:
 		panic("wat should I do")
 	}
 }
 
+func createBridge(pid string) {
+
+	bridgeIP, bridgeSubnet, err := net.ParseCIDR("10.10.10.1/24")
+	check(err)
+
+	containerIP, _, err := net.ParseCIDR("10.10.10.2/24")
+	check(err)
+
+	netConfig := network.NetworkConfig{
+		BridgeName:     "brgtest",
+		BridgeIP:       bridgeIP,
+		ContainerIP:    containerIP,
+		Subnet:         bridgeSubnet,
+		VethNamePrefix: "vethtest",
+	}
+	processID, err := strconv.Atoi(pid)
+	check(err)
+
+	bridge := network.NewBridge()
+	veth := network.NewVeth()
+	network.ApplyHost(brdige, veth, netConfig, processID)
+	network.ApplyContainer(netConfig, processID)
+}
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -32,6 +60,7 @@ func parent() {
 		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
+	log.Println(cmd.Process.Pid)
 }
 
 func child() {
@@ -46,4 +75,11 @@ func child() {
 		os.Exit(1)
 	}
 	log.Println("app:", cmd.Process.Pid)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Printf("ERROR - %s\n", err.Error())
+		os.Exit(1)
+	}
 }
---
+++ network/network.go
@@ -0,0 +1,71 @@
+package network
+
+import (
+	"github.com/teddyking/netsetgo"
+)
+type NetworkConfig struct {
+	BridgeName     string
+	BridgeIP       net.IP
+	ContainerIP    net.IP
+	Subnet         *net.IPNet
+	VethNamePrefix string
+}
+
+func ApplyHost(bridge *Bridge,veth *Veth,netConfig NetworkConfig,  pid int) error {
+	bridge, err := bridge.Create(netConfig.BridgeName, netConfig.BridgeIP, netConfig.Subnet)
+	if err != nil {
+		return err
+	}
+
+	hostVeth, containerVeth, err := veth.Create(netConfig.VethNamePrefix)
+	if err != nil {
+		return err
+	}
+
+	err = h.BridgeCreator.Attach(bridge, hostVeth)
+	if err != nil {
+		return err
+	}
+
+	err = h.VethCreator.MoveToNetworkNamespace(containerVeth, pid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func ApplyContainer(netConfig NetworkConfig, pid int) error {
+	netnsFile, err := os.Open(fmt.Sprintf("/proc/%d/ns/net", pid))
+	defer netnsFile.Close()
+	if err != nil {
+		return fmt.Errorf("Unable to find network namespace for process with pid '%d'", pid)
+	}
+
+	cbFunc := func() error {
+		containerVethName := fmt.Sprintf("%s1", netConfig.VethNamePrefix)
+		link, err := netlink.LinkByName(containerVethName)
+		if err != nil {
+			return fmt.Errorf("Container veth '%s' not found", containerVethName)
+		}
+
+		addr := &netlink.Addr{IPNet: &net.IPNet{IP: netConfig.ContainerIP, Mask: netConfig.Subnet.Mask}}
+		err = netlink.AddrAdd(link, addr)
+		if err != nil {
+			return fmt.Errorf("Unable to assign IP address '%s' to %s", netConfig.ContainerIP, containerVethName)
+		}
+
+		if err := netlink.LinkSetUp(link); err != nil {
+			return err
+		}
+
+		route := &netlink.Route{
+			Scope:     netlink.SCOPE_UNIVERSE,
+			LinkIndex: link.Attrs().Index,
+			Gw:        netConfig.BridgeIP,
+		}
+
+		return netlink.RouteAdd(route)
+	}
+
+	return c.NetnsExecer.Exec(netnsFile, cbFunc)
+}
